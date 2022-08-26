f5de0ab27fe00077855ad170524961bfcb871cbe
xinli <lixin@cailian.net>
2022-08-26 11:17:33 +0800

    没有给权限会崩溃

--- LiveDemo/src/main/java/com/xin/livedemo/MainActivity.java
+++ LiveDemo/src/main/java/com/xin/livedemo/MainActivity.java
@@ -1,15 +1,21 @@
 package com.xin.livedemo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private Button mPushBtn;
     private Button mPlayBtn;
+    private boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +25,29 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mPlayBtn = (Button) findViewById(R.id.play_stream_btn);
         mPushBtn.setOnClickListener(this);
         mPlayBtn.setOnClickListener(this);
+        int storage = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int camera = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int audio = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int internet = ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        if (storage == PackageManager.PERMISSION_GRANTED
+                && camera == PackageManager.PERMISSION_GRANTED
+                && audio == PackageManager.PERMISSION_GRANTED
+                && internet == PackageManager.PERMISSION_GRANTED) {
+            flag = true;
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.push_stream_btn:
-                startActivity(new Intent(this,CameraActivity.class));
+                if (flag)
+                    startActivity(new Intent(this, CameraActivity.class));
+                else
+                    Toast.makeText(this, "请先给予应用权限", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.play_stream_btn:
-                startActivity(new Intent(this,PlayerActivity.class));
+                startActivity(new Intent(this, PlayerActivity.class));
                 break;
         }
     }
